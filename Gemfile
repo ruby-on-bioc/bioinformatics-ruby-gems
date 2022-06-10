b81f9bc6fda7a7bba2dd98f38511d8ea2b74f68e
kojix2 <2xijok@gmail.com>
2022-06-10 11:27:16 +0900

    Make messages friendlier

--- Gemfile
+++ Gemfile
@@ -2,4 +2,7 @@
 
 source 'https://rubygems.org'
 
+gem 'erb'
 gem 'gems'
+gem 'tty-progressbar'
+gem 'tty-spinner'
--- generate.rb
+++ generate.rb
@@ -1,33 +1,61 @@
+#! /usr/bin/env ruby
 # frozen_string_literal: true
 
 require 'gems'
 require 'erb'
+require 'tty-progressbar'
+require 'tty-spinner'
 
-bio_gems = []
+bio_gem_names = []
+spinner = TTY::Spinner.new('[:spinner] :title')
+spinner.update(title: 'Search for Bio Gems')
+spinner.auto_spin
 i = 1
 
 loop do
   arr = Gems.search('bio-', page: i)
-  bio_gems.concat(arr)
+  names = arr.map { _1['name'] }
+  bio_gem_names += names
   i += 1
   sleep 0.5
   break unless arr.size == 30
 end
+spinner.success("(#{bio_gem_names.size} gems found)")
 
-# EXCLUDE LIST
+spinner.update(title: 'EXCLUDE LIST')
+spinner.auto_spin
 exclude_list = File.readlines('exclude_list.txt').map(&:chomp)
+bio_gem_names -= exclude_list
+spinner.success "#{bio_gem_names.size} gems left"
 
-bio_gems.filter! do |r|
-  !exclude_list.include? r['name']
+spinner.update(title: 'INCLUDE LIST')
+spinner.auto_spin
+include_list = File.readlines('include_list.txt').map(&:chomp)
+bio_gem_names += include_list
+spinner.success "#{bio_gem_names.size} gems left"
+
+bar = TTY::ProgressBar.new(
+  'Get GEM INFO [:bar]', total: bio_gem_names.size, bar_foramt: :block
+)
+
+bio_gems = []
+
+bio_gem_names.each do |name|
+  info = Gems.info(name)
+  bio_gems << info
+  sleep 0.5
+  bar.advance
 end
 
-# TITLE
+bar.finish
+
+warn 'Title'
 puts '# Bio Gems'
 puts
 
-erb = ERB.new(File.read('table.erb'), trim_mode: '-')
+erb = ERB.new(File.read('field.erb'), trim_mode: '-')
 
-# Table 
+warn 'Fields'
 bio_gems.each do |r|
   puts erb.result(binding)
 end
