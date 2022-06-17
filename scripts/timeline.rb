eeb61ec12b387b7e75038acc3874e95989472fd2
kojix2 <2xijok@gmail.com>
2022-06-17 14:11:13 +0900

    Rubocop auto correct

--- scripts/timeline.rb
+++ scripts/timeline.rb
@@ -14,20 +14,19 @@ spinner.update(title: 'Fetch Gem info')
 spinner.auto_spin
 
 a.map do |h|
-  n =  h['name']
+  n = h['name']
   r = Gems.versions(n)
   r.each do |g|
-    dt = DateTime.parse(g["created_at"])
+    dt = DateTime.parse(g['created_at'])
     dates << [n, dt.day, dt.month, dt.year]
   end
 end
 
 spinner.success
 
-CSV.open(File.expand_path("timeline.csv", __dir__), "wb") do |csv|
-  csv << ["gems", "day", "month", "year"]
+CSV.open(File.expand_path('timeline.csv', __dir__), 'wb') do |csv|
+  csv << %w[gems day month year]
   dates.each do |d|
     csv << d
   end
 end
-
